300e1ce0e087f2bc7c6087e0f31b9b80ca96a31d
matty <matty91@gmail.com>
2016-10-03 11:10:07 -0400

    Added exception handling and cleaned up various pylint errors.

--- dnslatency.py
+++ dnslatency.py
@@ -1,49 +1,71 @@
-import random
+""" DNS latency checking module written for Ganglia. """
+
+import sys
 import socket
 import dns.resolver
+from datetime import datetime
 from timeit import default_timer as timer
 
 DEBUG = 0
 descriptors = []
 
+
+def lookup_failure(status_code, domain, name_server, dns_answer):
+    """
+       Routine to call if a DNS lookup fails.
+    """
+    print "%s: Unable to resolve %s from %s at %s" % (status_code, 
+                                                      domain, 
+                                                      name_server, 
+                                                      str(datetime.now()))
+    if dns_answer:
+        dns_answer.response.to_text()
+
+
 def resolve_name(name):
-    """ 
-        Convert a name to an IP address 
+    """
+        Convert a name to an IP address
     """
     try:
-        ip = socket.gethostbyname(name)
+        ip_addr = socket.gethostbyname(name)
     except socket.herror:
-        ip = "8.8.8.8"
-    return ip
+        print "Unable to resolve " + name + " to an IP address"
+        sys.exit(1)
+    return ip_addr
 
-def time_name_resolution(name_server, name, rr_type):
+def time_name_resolution(name_server, domain, rr_type):
     """
        Calculate the time it takes to resolve a domain name
     """
+    dns_answer = None
     resolver = dns.resolver.Resolver()
     resolver.nameservers = name_server
-    
     start_time = timer()
+
     try:
-        dns_answer = resolver.query(name, rr_type)
-    except:
-        # TODO: Enhance error handling to log DNS response headers fields 
-        # if a result isn't returned. This information can be used as an
-        # index into a tcpdump cyclical buffer collection to pinpoint issues.
+        dns_answer = resolver.query(domain, rr_type)
+    except dns.resolver.NXDOMAIN:
+        lookup_failure("NXDOMAIN", domain, name_server, dns_answer)
+        return 60
+    except dns.resolver.NoAnswer:
+        lookup_failure("NOANSWER", domain, name_server, dns_answer)
+        return 60
+    except dns.exception.Timeout:
+        lookup_failure("TIMEOUT", domain, name_server, dns_answer)
         return 60
 
-    end_time = timer()        
-    return (end_time - start_time)
+    end_time = timer()
+    return end_time - start_time
 
 def query_handler(name):
-    """ 
+    """
        Callback invoked by ganglia to ocollect metrics
     """
     name_servers = []
     metric, dns_server_name, domain, rrec = name.split('_')
     name_servers.append(resolve_name(dns_server_name))
-    resolution_time =  time_name_resolution(name_servers, domain, rrec)
-    
+    resolution_time = time_name_resolution(name_servers, domain, rrec)
+
     if DEBUG:
         print "DNSINFO: Query handler variables:"
         print "  DNSINFO: DNS Server name " + dns_server_name
@@ -56,29 +78,33 @@ def query_handler(name):
 
 
 def metric_init(params):
-    for site, values in params.items():
+    for param_name, values in params.items():
         dns_server_name, domain, rrec = values.split()
 
-        desc = { 'name': 'dnslatency_' + dns_server_name + '_' + domain + '_' + rrec,
-                 'call_back': query_handler,
-                 'time_max': 60,
-                 'value_type': 'double',
-                 'units': 'Query Time',
-                 'slope': 'both',
-                 'format': '%.4f',
-                 'description': 'DNS resolution time',
-                 'groups': 'dns_latency'
-               }
-        
+        desc = {
+            'name': 'dnslatency_' + dns_server_name + '_' + domain + '_' + rrec,
+            'call_back': query_handler,
+            'time_max': 60,
+            'value_type': 'double',
+            'units': 'Query Time',
+            'slope': 'both',
+            'format': '%.4f',
+            'description': 'DNS resolution time',
+            'groups': 'dns_latency'
+        }
+
         descriptors.append(desc)
 
     return descriptors
 
-def Main():
-    # Code to test the script w/o involving ganglia
-    params = { 'googlednsa_dns_resolution': 'google-public-dns-a.google.com google.com A',
-               'googlednsb_dns_resolution': 'google-public-dns-b.google.com google.com A',
-             }
+def main():
+    """
+       Code to test the script w/o involving ganglia
+    """
+    params = {
+        'googlednsa_dns_resolution': 'google-public-dns-a.google.com prefetch.net A'
+        #'googlednsb_dns_resolution': 'google-public-dns-b.google.com google.prefetch.net A',
+    }
     descriptors = metric_init(params)
     for desc in descriptors:
         latency = desc['call_back'](desc['name'])
@@ -87,4 +113,4 @@ def Main():
 
 
 if __name__ == "__main__":
-    Main()
+    main()
